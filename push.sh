36f33bfb205aabb0428ac131245518d86918e8ae
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2022-10-05 16:19:23 +0200

    fix

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build . -t docker.io/luckysideburn/vaultbot:v1.1.2
-docker push docker.io/luckysideburn/vaultbot:v1.1.2
+docker build . -t docker.io/luckysideburn/vaultbot:v1.1.3
+docker push docker.io/luckysideburn/vaultbot:v1.1.3
 git add .
 git commit -m "fix"
 git push origin main
